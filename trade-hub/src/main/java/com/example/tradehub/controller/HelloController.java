40a1c53ff4dac311af526143bbfa89e6d420323f
XF <2194684488@qq.com>
2026-04-15 10:33:43 +0800

    feat: 添加第一个 Controller 接口 (HelloController)

--- trade-hub/src/main/java/com/example/tradehub/controller/HelloController.java
+++ trade-hub/src/main/java/com/example/tradehub/controller/HelloController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class HelloController {
     @GetMapping("/hi")
     public String hello(){
 
-        return "你好，勾八郭";
+        return "你好，世界";
     }
 }
